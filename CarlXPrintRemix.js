4d9ded8760288882934c1049e5dcccfe3877e31c
James Staub <james.staub@nashville.gov>
2017-07-04 02:49:08 -0500

    Building transit slip (incomplete)

--- CarlXPrintRemix.js
+++ CarlXPrintRemix.js
@@ -30,28 +30,79 @@ while (!WScript.StdIn.AtEndOfStream)
 
 // TRANSIT SLIP
 
+
+// HOLD
+var hold = true;
 /*
 // DESTINATION BRANCH 
 var re = /\n.*SEND TO: (.{2,5})/;
 var matches = re.exec(printed);
 var destination = matches[1];
-if (destination.length == 5) { destination = substr(destination,2,3); }
+// Set MNPS header
+// if (destination.length == 5) { destination = substr(destination,2,3); }
+if (destination.length == 5) { destination = "MNPS"; }
+// Set School label
+
+// PATRON GUID? BARCODE
+
+// ITEM ID FINAL 4 DIGITS
+
+// PULL FROM HOLDSHELF DATE
+
+// PATRON LAST NAME
+
+// PATRON FIRST NAME
+
 */
 
-// PRINT
+// COMPOSE PRINTED PAGE
 // EPSON ESC/POS information at http://www.epsonexpert.com/Epson_Assets/ESCPOS_Commands_FAQs.pdf
+// AND http://download.delfi.com/SupportDL/Epson/Manuals/TM-T88IV/Programming%20manual%20APG_1005_receipt.pdf
 newPrint += "\x1B" + "@" // Initialize printer
 
-newPrint += "------------------------------------------\n";
+//newPrint += printed + "\n";
 
-newPrint += printed + "\n";
+// TRANSIT AREA
+newPrint += "\x1B" + "a" + "\x01"; // centered
+newPrint += "\x1D" + "!" + "\x33"; // quad-width, quad-height
+// HOLD
+if (hold) {
+	newPrint += "\x1D" + "B" + "\x01"; // reverse black and white
+	newPrint += "   HOLD   \n";
+	newPrint += "\x1D" + "B" + "\x00"; // cancel reverse black and white
+}
+// DESTINATION BRANCH 
+//newPrint += destination + "\n";
+newPrint += "\x1D" + "!" + "\x00"; // cancel quad-width, quad-height
+newPrint += "\x1B" + "a" + "\x00"; // left-justified
 
-//newPrint += "DESTINATION: " + destination + "\n";
+// PATRON GUID? BARCODE
+
+// ITEM ID FINAL 4 DIGITS
+
+// PULL FROM HOLDSHELF DATE
+
+// PATRON FIRST NAME
+newPrint += "\x1B" + "V" + "\x01"; // 90 degree clockwise rotation
+newPrint += "\x1D" + "!" + "\x22"; // double-width, double-height
+newPrint += nameFirst + "\n";
+newPrint += "\x1D" + "!" + "\x00"; // cancel quad-width, quad-height
+newPrint += "\x1B" + "V" + "\x01"; // cancel 90 degree clockwise rotation
+
+// PATRON LAST NAME
+newPrint += "\x1B" + "V" + "\x01"; // 90 degree clockwise rotation
+newPrint += "\x1D" + "!" + "\x22"; // triple-width, triple-height
+newPrint += nameLast + "\n";
+newPrint += "\x1D" + "!" + "\x00"; // cancel quad-width, quad-height
+newPrint += "\x1B" + "V" + "\x01"; // cancel 90 degree clockwise rotation
 
 newPrint += "------------------------------------------\n";
 newPrint +=  "\n"; // necessary?
 newPrint +=  "\n"; // necessary?
 newPrint += "\x1D" + "V" + "\x42"; // feed, partial cut
+
+
+// PRINT!
 
 WScript.StdOut.WriteLine("JAMES LOOK HERE->");
 WScript.StdOut.WriteLine(newPrint);
